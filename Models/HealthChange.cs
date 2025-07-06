1e3cac401eca30b306cc4debd24b04f531ecf0ed
Rickard Nilsson Örjemark <rickardni@gmail.com>
2025-07-06 23:33:07 +0200

    Refactor history overlay in Home component to use History component
    - Replaced the existing history overlay implementation with a new History component for improved modularity and maintainability.
    - Removed redundant CSS styles related to the history overlay, streamlining the overall code structure.
    - Updated Home component to include necessary properties for the new History component.

---
+++ Models/HealthChange.cs
@@ -0,0 +1,9 @@
+namespace Kol.Models
+{
+    public class HealthChange
+    {
+        public bool IsTopPlayer { get; set; }
+        public int ChangeAmount { get; set; }
+        public int NewHealth { get; set; }
+    }
+} 
